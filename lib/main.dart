cbe5d1d49760280e5b228cf649cf1470a5f70370
= <=>
2025-10-07 10:43:36 +0700

    darktheme login dll

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,64 @@ import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
 import 'screens/home_screen.dart';
+import 'controllers/register_controller.dart';
 
 void main() {
-  runApp(const MyApp());
+  final registerController = RegisterController();
+  runApp(MyApp(registerController: registerController));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  final RegisterController registerController;
+  const MyApp({super.key, required this.registerController});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+
+  // 🔹 akses global supaya bisa dipanggil dari halaman lain
+  static _MyAppState? of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  // ✅ controller global
+  final RegisterController _registerController = RegisterController();
+
+  void changeTheme(ThemeMode themeMode) {
+    setState(() {
+      _themeMode = themeMode;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+
+      // Tema terang
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const LoginScreen(),
 
+      // Tema gelap
+      darkTheme: ThemeData.dark(),
+
+      // 🔹 themeMode bisa berubah lewat PengaturanScreen
+      themeMode: _themeMode,
+
+      // ✅ Pass controller ke LoginScreen
+      home: LoginScreen(registerController: _registerController),
       routes: {
         '/home': (context) => const HomeScreen(),
-        '/register': (context) => const RegisterScreen(),
+
+        // ✅ Pass controller ke RegisterScreen
+        '/register':
+            (context) =>
+                RegisterScreen(registerController: _registerController),
       },
     );
   }
