0ce3e53f8be641da3405828be89724673cdc5eec
= <=>
2025-11-01 19:45:02 +0700

    Tingkatkan kompatibilitas Java dan Kotlin ke versi 21, tambahkan file local.properties, perbarui dependensi package_info_plus ke versi 9.0.0, dan sesuaikan pembuatan status AddExpenseScreen.

--- lib/screens/add_expense_screen.dart
+++ lib/screens/add_expense_screen.dart
@@ -9,6 +9,7 @@ class AddExpenseScreen extends StatefulWidget {
   const AddExpenseScreen({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _AddExpenseScreenState createState() => _AddExpenseScreenState();
 }
 
