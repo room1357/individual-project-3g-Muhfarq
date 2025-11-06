48f9624b598147571031dfecb124316b5f704871
= <=>
2025-11-06 17:48:38 +0700

    Install dan config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; 
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/splash_screen.dart';
 
-void main() {
+void main() async {
+  // Pastikan binding sudah siap sebelum async call
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 🔥 Inisialisasi Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Jalankan aplikasi
   runApp(const MyApp());
 }
 
@@ -14,7 +23,6 @@ class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
 
-  // 🔹 Untuk ganti tema dari halaman lain (kalau mau)
   static _MyAppState? of(BuildContext context) =>
       context.findAncestorStateOfType<_MyAppState>();
 }
@@ -32,7 +40,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Expense Manager',
 
       // Tema terang
       theme: ThemeData(
@@ -42,14 +50,11 @@ class _MyAppState extends State<MyApp> {
 
       // Tema gelap
       darkTheme: ThemeData.dark(),
-
-      // Bisa diganti lewat fungsi changeTheme
       themeMode: _themeMode,
 
-      // Halaman awal: splash screen
+      // Halaman awal
       home: const SplashScreen(),
 
-      // Routes
       routes: {
         '/home': (context) => const HomeScreen(),
         '/register': (context) => const RegisterScreen(),
